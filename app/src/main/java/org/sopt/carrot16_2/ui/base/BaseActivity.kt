c5c993221a492a8b624c1e07c0572bc8d80379d5
Jaehun-Jo <chojh922@gmail.com>
2022-05-16 15:35:12 +0900

    #1 [chore] 프로젝트 초기 세팅

---
+++ app/src/main/java/org/sopt/carrot16_2/ui/base/BaseActivity.kt
@@ -0,0 +1,18 @@
+package org.sopt.carrot16_2.ui.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int,
+) : AppCompatActivity() {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+    }
+}
---
+++ app/src/main/java/org/sopt/carrot16_2/ui/create/CreateActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.carrot16_2.ui.create
+
+import android.os.Bundle
+import org.sopt.carrot16_2.R
+import org.sopt.carrot16_2.databinding.ActivityCreateBinding
+import org.sopt.carrot16_2.ui.base.BaseActivity
+
+class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_create) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
