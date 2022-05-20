35c2028dd468de761494e6a0d14866d2147bb9e8
hyeonjeong Park <py0429@ewhain.net>
2022-05-20 23:07:44 +0900

    리사이클러뷰, createActivity 구현

--- app/src/main/java/org/sopt/carrot16_2/ui/create/CreateActivity.kt
+++ app/src/main/java/org/sopt/carrot16_2/ui/create/CreateActivity.kt
@@ -1,6 +1,7 @@
 package org.sopt.carrot16_2.ui.create
 
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import org.sopt.carrot16_2.R
 import org.sopt.carrot16_2.databinding.ActivityCreateBinding
 import org.sopt.carrot16_2.model.ImageData
@@ -11,17 +12,38 @@ class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_cre
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //initAdapter()
+        initAdapter()
+        changeEvent()
+
     }
 
     private fun initAdapter(){
+        createImageAdapter = CreateImageAdapter()
+        val image : String = "https://images.theconversation.com/files/457052/original/file-20220408-15-pl446k.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip"
         binding.rvImage.adapter = createImageAdapter
         createImageAdapter.imageList.addAll(
             listOf(
-                ImageData("R.drawable.ic_launcher_foreground")
+                ImageData(image),
+                ImageData(image),
+                ImageData(image)
             )
         )
         createImageAdapter.notifyDataSetChanged()
+    }
+
+    private fun changeEvent() {
+        binding.etMoney.addTextChangedListener {
+            if (!binding.etMoney.text.isEmpty()) {
+                binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_black2))
+                binding.tvWon.setTextColor(getColor(R.color.carrot_and_black2))
+            } else {
+                binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_squaregray))
+                binding.tvWon.setTextColor(getColor(R.color.carrot_and_squaregray))
+            }
+        }
+        binding.btnRadio.setOnClickListener{
+            binding.btnRadio?.isSelected = binding.btnRadio.isSelected==false
+        }
     }
 
 }
