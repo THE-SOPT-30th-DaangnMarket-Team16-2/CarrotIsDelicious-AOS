8583462540dc380ea98ce1264b427a37fedeec31
hyeonjeong Park <py0429@ewhain.net>
2022-05-22 21:50:01 +0900

    merge

--- app/src/main/java/org/sopt/carrot16_2/ui/create/CreateActivity.kt
+++ app/src/main/java/org/sopt/carrot16_2/ui/create/CreateActivity.kt
@@ -1,6 +1,8 @@
 package org.sopt.carrot16_2.ui.create
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.core.widget.addTextChangedListener
 import org.sopt.carrot16_2.R
 import org.sopt.carrot16_2.databinding.ActivityCreateBinding
@@ -9,6 +11,7 @@ import org.sopt.carrot16_2.ui.base.BaseActivity
 
 class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_create) {
     private lateinit var createImageAdapter: CreateImageAdapter
+    private var radioButtonState = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,17 +35,32 @@ class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_cre
     }
 
     private fun changeEvent() {
-        binding.etMoney.addTextChangedListener {
-            if (!binding.etMoney.text.isEmpty()) {
-                binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_black2))
-                binding.tvWon.setTextColor(getColor(R.color.carrot_and_black2))
-            } else {
-                binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_squaregray))
-                binding.tvWon.setTextColor(getColor(R.color.carrot_and_squaregray))
+        /*binding.etMoney.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {//커서 올렸을때
+
             }
-        }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {//바뀌는 도중
+                if(s?.length!! >0){
+                    binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_black2))
+                    binding.tvWon.setTextColor(getColor(R.color.carrot_and_black2))
+                }else {
+                    binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_squaregray))
+                    binding.tvWon.setTextColor(getColor(R.color.carrot_and_squaregray))
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {//아에 바뀐후
+            }
+        })*/
         binding.btnRadio.setOnClickListener{
             binding.btnRadio?.isSelected = binding.btnRadio.isSelected==false
+            radioButtonState = !radioButtonState
+            if(radioButtonState) {
+                binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_black2))
+            } else {
+                binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_squaregray))
+            }
         }
     }
 
--- app/src/main/java/org/sopt/carrot16_2/ui/main/HomeActivity.kt
+++ app/src/main/java/org/sopt/carrot16_2/ui/main/HomeActivity.kt
@@ -5,7 +5,7 @@ import org.sopt.carrot16_2.R
 import org.sopt.carrot16_2.databinding.ActivityHomeBinding
 import org.sopt.carrot16_2.ui.base.BaseActivity
 
-class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+class  HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
