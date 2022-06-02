67215d16762d9774fa8a49bdf0cf5e772a380497
hyeonjeong Park <py0429@ewhain.net>
2022-06-02 15:41:58 +0900

    #35 [server] homeActivity api 구현

--- app/src/main/java/org/sopt/carrot16_2/ui/main/HomeActivity.kt
+++ app/src/main/java/org/sopt/carrot16_2/ui/main/HomeActivity.kt
@@ -2,13 +2,15 @@ package org.sopt.carrot16_2.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import org.sopt.carrot16_2.R
+import org.sopt.carrot16_2.data.remote.RetrofitBuilder
 import org.sopt.carrot16_2.databinding.ActivityHomeBinding
-import org.sopt.carrot16_2.model.SellData
 import org.sopt.carrot16_2.ui.base.BaseActivity
 import org.sopt.carrot16_2.ui.create.CreateActivity
 import org.sopt.carrot16_2.ui.main.viewmodel.HomeViewModel
+import org.sopt.carrot16_2.util.enqueueUtil
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
     private lateinit var sellAdapter: SellAdapter
@@ -25,14 +27,18 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
     private fun initAdapter() {
         sellAdapter = SellAdapter()
         binding.rvHomeContent.adapter = sellAdapter
-        sellAdapter.dataList.addAll(
-            listOf(
-                SellData("강아지 샘플 사료를 판매합니다", "개봉동", "9", "3,000"),
-                SellData("마스크 판매합니다", "구로구 신도림동", "10", "8,000"),
-                SellData("크림 세트 판매합니다", "개봉동", "12", "24,000"),
-                SellData("강아지 사료 판매합니다", "개봉동", "12", "10,000"),
-                SellData("여름 자켓 판매", "개봉동", "13", "16,000")
-            )
+        val call = RetrofitBuilder.homeService.getHomeFeed()
+        call.enqueueUtil(
+            onSuccess = {
+                sellAdapter.dataList.clear()
+                sellAdapter.dataList.addAll(it.data)
+                sellAdapter.notifyDataSetChanged()
+            },
+            onError = {
+                when (it) {
+                    500 -> Toast.makeText(this, "서버 내부 오류", Toast.LENGTH_SHORT).show()
+                }
+            }
         )
 
     }
