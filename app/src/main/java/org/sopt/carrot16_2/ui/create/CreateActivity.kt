3be8d7d3d0c2cb5ccbbf722b7a19eb17d10265dd
hyeonjeong Park <py0429@ewhain.net>
2022-06-02 22:38:34 +0900

    #37 [feat] camera 리사이클러뷰랑 연결

--- app/src/main/java/org/sopt/carrot16_2/ui/create/CreateActivity.kt
+++ app/src/main/java/org/sopt/carrot16_2/ui/create/CreateActivity.kt
@@ -1,10 +1,18 @@
 package org.sopt.carrot16_2.ui.create
 
+import android.Manifest
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import org.sopt.carrot16_2.R
 import org.sopt.carrot16_2.data.remote.RetrofitBuilder
 import org.sopt.carrot16_2.data.remote.entity.request.CreateRequest
@@ -21,6 +29,15 @@ class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_cre
     private lateinit var createImageAdapter : CreateImageAdapter
     private var radioButtonState = false
     private val createViewModel by viewModels<CreateViewModel>()
+    var photoUri : Uri? = null
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                navigateGallery()
+            } else {
+                Toast.makeText(this,"갤러리 접근 실패",Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,42 +49,15 @@ class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_cre
         changeEvent()
         initFinishBtnClickListener()
         //completeEvent()
+        imageUpload()
     }
 
     private fun initAdapter() {
         createImageAdapter = CreateImageAdapter()
-        val image: String =
-            "https://images.theconversation.com/files/457052/original/file-20220408-15-pl446k.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip"
         binding.rvImage.adapter = createImageAdapter
-        createImageAdapter.imageList.addAll(
-            listOf(
-                ImageData(image),
-                ImageData(image),
-                ImageData(image)
-            )
-        )
-        createImageAdapter.notifyDataSetChanged()
     }
 
     private fun changeEvent() {
-        /*binding.etMoney.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {//커서 올렸을때
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {//바뀌는 도중
-                if(s?.length!! >0){
-                    binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_black2))
-                    binding.tvWon.setTextColor(getColor(R.color.carrot_and_black2))
-                }else {
-                    binding.tvSugMoney.setTextColor(getColor(R.color.carrot_and_squaregray))
-                    binding.tvWon.setTextColor(getColor(R.color.carrot_and_squaregray))
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {//아에 바뀐후
-            }
-        })*/
         binding.btnRadio.setOnClickListener {
             binding.btnRadio?.isSelected = binding.btnRadio.isSelected == false
             radioButtonState = !radioButtonState
@@ -113,5 +103,68 @@ class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_cre
         }
     }
 
+    /**
+     * 카메라 연결
+     */
+    private fun imageUpload() {
+        //버튼 클릭시 갤러리 접근하여 이미지 uri 전달
+        binding.clCamera.setOnClickListener {
+            if(createImageAdapter.itemCount < 10){
+                when {
+                    ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED -> {
+                        navigateGallery()
+                    }
+                    shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                        showInContextUI()
+                    }
+                    else -> {
+                        requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this,"이미지 개수 10개 초과",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    private fun navigateGallery() {
+        val photoIntent = Intent(Intent.ACTION_PICK) //open the album
+        photoIntent.type = "image/*"
+        openGallery.launch(photoIntent)
+    }
+
+
+    private val openGallery : ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){ result ->
+        if(result.resultCode == RESULT_OK && result.data != null){
+            photoUri = result.data?.data
+            createImageAdapter.imageList.add(ImageData(photoUri.toString()))
+            createImageAdapter.notifyDataSetChanged()
+            binding.tvPickedImage.text = createImageAdapter.itemCount.toString()
+
+
+        }else if(result.resultCode == RESULT_CANCELED){
+            Toast.makeText(this,"사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun showInContextUI() {
+        AlertDialog.Builder(this)
+            .setTitle("권한 동의 필요")
+            .setMessage("프로필 사진 수정을 위해 갤러리 접근 권한이 필요합니다.")
+            .setPositiveButton("동의") { _, _ ->
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+            .setNegativeButton("거부") { _, _ ->
+                Toast.makeText(this, "갤러리 접근 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+            .create()
+            .show()
+    }
 
 }
